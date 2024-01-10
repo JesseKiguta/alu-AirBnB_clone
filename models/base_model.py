bac77173a98e65f84e7b045adb09a4073996067a
danjor667 <jordannguepi@gmail.com>
2024-01-10 14:19:33 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,33 @@ class BaseModel:
     Base Model
     """
     def __init__(self):
-        id = str(uuid.uuid4())
-        created_at = datetime.now().isoformat()
-        updated_at = datetime.now().isoformat()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
 
     def __str__(self):
-        pass
+        """
+        string representation of the obj
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
 
     def save(self):
-        pass
+        """
+        update the Updated_at field
+        """
+        self.updated_at = datetime.now().isoformat()
 
 
     def to_dict(self):
-        pass
+        """
+        returns a dictionary representation of 
+        the obj
+        """
+        my_dict = {}
+        print(self.__dict__)
+        for k, v in self.__dict__.items():
+                my_dict[k] = v
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
