f3163f7427b055b8ca2c240833f097c2d695b747
danjor667 <jordannguepi@gmail.com>
2024-01-11 14:23:19 +0200

    console

--- console.py
+++ console.py
@@ -6,4 +6,39 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    """
+    Main class
+    """
+    prompt = "(hbnb) "
+
+
+    def do_quit(self, arg):
+        """
+        exiting the console
+        """
+        return True
+
+    def emptyline(self):
+        """
+        do nothing
+        """
+        pass
+
+    def do_EOF(self, args):
+        """
+        keyboard interruption
+        """
+        return True
+
+    def precmd(self, arg):
+        """
+        preprocess the arg
+        """
+
+        return arg
+
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
+
