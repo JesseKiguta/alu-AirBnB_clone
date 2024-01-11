c370984f54d7ccbc73d1c973df39105de5a926d7
JesseKiguta <j.kiguta@alustudent.com>
2024-01-11 21:56:10 +0200

    Import models to console.py

--- console.py
+++ console.py
@@ -3,6 +3,13 @@
 console
 """
 import cmd
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,6 +17,9 @@ class HBNBCommand(cmd.Cmd):
     Main class
     """
     prompt = "(hbnb) "
+    models = {'BaseModel': BaseModel, 'Amenity': Amenity,
+               'City': City, 'Place': Place, 'Review': Review,
+               'State': State, 'User': User}
 
 
     def do_quit(self, arg):
