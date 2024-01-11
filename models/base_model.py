2b7572af7ef19c8dc298e9c27d825d2ecbdadcc5
danjor667 <jordannguepi@gmail.com>
2024-01-11 11:00:27 +0200

    update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,19 @@ class BaseModel:
     """
     Base Model
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.fromisoformat(v))
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
 
     def __str__(self):
