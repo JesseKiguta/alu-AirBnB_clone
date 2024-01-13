6ddbfff3b721ea4d9ba058a3867141d744f4bf58
danjor667 <jordannguepi@gmail.com>
2024-01-13 12:01:29 +0200

    tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,12 @@
+from models.base_model import BaseModel
+from models.amenity import Amenity
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
