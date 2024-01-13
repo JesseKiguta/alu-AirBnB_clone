0a966637890f01609dff7de77dfc53b7b641ebc3
danjor667 <jordannguepi@gmail.com>
2024-01-13 13:05:20 +0200

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,11 +1,41 @@
+"""
+test amenity
+"""
 from models.base_model import BaseModel
 from models.amenity import Amenity
+from datetime import datetime
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        pass
+class TestAmenity(unittest.TestCase):
+    """
+    test Amenity
+    """
+    def test_isBaseModelInstance(self):
+        """
+        test if it is an instance of BaseModel
+        """
+        inst = Amenity()
+        self.assertIsInstance(inst, BaseModel)
+
+    def test_attr(self):
+        """
+        testing the attr
+        """
+        inst = Amenity()
+        self.assertTrue(hasattr(inst, "id"))
+        self.assertTrue(hasattr(inst, "created_at"))
+        self.assertTrue(hasattr(inst, "updated_at"))
+        self.assertTrue(hasattr(inst, "name"))
+
+    def test_name(self):
+        """
+        testing the name attr
+        """
+        inst = Amenity()
+        self.assertTrue(type(inst.name), str)
+        self.assertTrue(isinstance(inst.created_at, datetime))
+        self.assertTrue(isinstance(inst.id, str))
 
 
 if __name__ == '__main__':
